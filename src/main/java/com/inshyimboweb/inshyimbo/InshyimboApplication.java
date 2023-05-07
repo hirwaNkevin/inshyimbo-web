5e96afaa8d62512ffca1058aa9e2e9f1e5cc6b16
hirwaNkevin <hirwankevin@gmail.com>
2023-05-07 20:36:15 +0200

    add project dependencies

---
+++ src/main/java/com/inshyimboweb/inshyimbo/InshyimboApplication.java
@@ -0,0 +1,13 @@
+package com.inshyimboweb.inshyimbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InshyimboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InshyimboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inshyimboweb/inshyimbo/InshyimboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inshyimboweb.inshyimbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InshyimboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
