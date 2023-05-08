1b05046ca0184499cc515ba1b81e0f2a4b01a715
hirwaNkevin <hirwankevin@gmail.com>
2023-05-08 11:57:28 +0200

    add Level service interface

---
+++ src/main/java/com/inshyimboweb/inshyimbo/service/LevelService.java
@@ -0,0 +1,10 @@
+package com.inshyimboweb.inshyimbo.service;
+
+import com.inshyimboweb.inshyimbo.model.Level;
+
+public interface LevelService {
+    public Level addLevel(Level level);
+    public Boolean deleteLevel(String Id);
+    public Level updateLevel(Level level);
+    public Level getLevel(String Id);
+}
