d3f2573accb8f73415aae82e0ef2bffdf3b1db2c
hirwaNkevin <hirwankevin@gmail.com>
2023-05-08 10:54:25 +0200

    update Leader model: add constructor, getters and setters

--- src/main/java/com/inshyimboweb/inshyimbo/model/Leader.java
+++ src/main/java/com/inshyimboweb/inshyimbo/model/Leader.java
@@ -9,4 +9,41 @@ public class Leader {
     private String fullName;
     private float rating;
     private int rates;
+    
+    public Leader() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public int getRates() {
+        return rates;
+    }
+
+    public void setRates(int rates) {
+        this.rates = rates;
+    }
+
+    
 }
