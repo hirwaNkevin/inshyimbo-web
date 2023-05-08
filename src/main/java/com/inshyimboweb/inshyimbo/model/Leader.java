61040d434756f94de8fd24f1b75e4c402b6e72d7
hirwaNkevin <hirwankevin@gmail.com>
2023-05-08 10:53:03 +0200

    update Leader model: Id type - int to String

--- src/main/java/com/inshyimboweb/inshyimbo/model/Leader.java
+++ src/main/java/com/inshyimboweb/inshyimbo/model/Leader.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Leader {
     @Id
-    private int Id;
+    private String Id;
     private String fullName;
     private float rating;
     private int rates;
