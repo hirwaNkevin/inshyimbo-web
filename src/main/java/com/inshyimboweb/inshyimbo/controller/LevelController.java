cd154f8a17c7b0ee4bb3bd2edf90fb08b4f590ad
hirwaNkevin <hirwankevin@gmail.com>
2023-05-08 14:30:59 +0200

    add Level Controller

---
+++ src/main/java/com/inshyimboweb/inshyimbo/controller/LevelController.java
@@ -0,0 +1,53 @@
+package com.inshyimboweb.inshyimbo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inshyimboweb.inshyimbo.model.Level;
+import com.inshyimboweb.inshyimbo.service.LevelService;
+
+@RestController
+@RequestMapping("/level")
+public class LevelController {
+    @Autowired
+    LevelService levelService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Level> saveLevel(@RequestBody Level level){
+        Level savedLevel = levelService.addLevel(level);
+        return new ResponseEntity<>(savedLevel, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/get")
+    public ResponseEntity<Level> getLevel(@RequestBody String Id){
+        Level savedLevel = levelService.getLevel(Id);
+        return new ResponseEntity<>(savedLevel, HttpStatus.FOUND);
+    }
+    
+    @PostMapping("/update")
+    public ResponseEntity<Level> updateLevel(@RequestBody Level level){
+        Level savedLevel = levelService.updateLevel(level);
+        return new ResponseEntity<>(savedLevel, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Boolean> deleteLevel(@RequestBody String Id){
+        Boolean isDeleted = levelService.deleteLevel(Id);
+        return new ResponseEntity<>(isDeleted, HttpStatus.OK);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Level>> findAll(){
+        List<Level> levels = levelService.findAll();
+        return new ResponseEntity<>(levels, HttpStatus.FOUND);
+    }
+}
