97455f99beb9a59f454092b15901ceec0034aec5
hirwaNkevin <hirwankevin@gmail.com>
2023-05-14 23:06:50 +0200

    create project-jar file

--- src/main/java/com/inshyimboweb/inshyimbo/InshyimboApplication.java
+++ src/main/java/com/inshyimboweb/inshyimbo/InshyimboApplication.java
@@ -2,10 +2,19 @@ package com.inshyimboweb.inshyimbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class InshyimboApplication {
 
+	@RequestMapping("/home")
+    public String index(Model model){
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(InshyimboApplication.class, args);
 	}
---
+++ src/main/java/com/inshyimboweb/inshyimbo/controller/IController.java
@@ -0,0 +1,19 @@
+package com.inshyimboweb.inshyimbo.controller;
+
+
+// import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/")
+public class IController {
+    
+    @GetMapping("/home")
+    public String index(Model model){
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
+}
